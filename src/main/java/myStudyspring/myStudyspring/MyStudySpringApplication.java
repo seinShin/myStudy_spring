e3bc33fb965b6c6da70b5ca4f8ade6e905513e9b
[seinShin] <tlstpdls13@gmail.com>
2023-05-23 18:13:30 +0900

    프로젝트 생성

---
+++ src/main/java/myStudyspring/myStudyspring/MyStudySpringApplication.java
@@ -0,0 +1,13 @@
+package myStudyspring.myStudyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStudySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myStudyspring/myStudyspring/MyStudySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package myStudyspring.myStudyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStudySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
